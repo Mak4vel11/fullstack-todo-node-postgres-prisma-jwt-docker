2542a43f7f456dda02976309555a188cdb0c4860
Luiz <luiz@gmail.com>
2025-09-17 14:23:14 +0200

    skema databases me 2 tavolina user dhe todos

---
+++ src/db.js
@@ -0,0 +1,26 @@
+import { DatabaseSync } from 'node:sqlite';
+const db = new DatabaseSync(':memory:')
+
+
+//setup database when boot app 
+//Execute SQL statemnts from string 
+//2 table 1 for user 1 for todo = that is gonna associate with the user 
+db.exec(`
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,                        
+        username TEXT UNIQUE,
+        password TEXT   
+    )
+    `)
+
+    db.exec(`
+            CREATE TABLE todos (
+               id INTEGER PRIMARY KEY AUTOINCREMENT ,
+               user_id INTEGER, 
+               task TEXT
+               completed BOOLEAN DEFAULT 0 ,
+               FOREIGN KEY(users_id) REFERENCES user(id)
+            )
+            `)
+
+            export default db
